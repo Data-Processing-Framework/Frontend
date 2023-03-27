c5bbf341f87c5da3c119b881bc04cf1850f1175f
martilopez <63054913+martilopez@users.noreply.github.com>
2023-03-27 13:44:59 +0200

    Graph
    sha afegit react flow i sha possat dintre el layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.sass';
 import './elements/css/colorPalette.sass'
 import { EditBar } from './elements/editBar';
-import {NavBar} from './elements/navBar'
+import { NavBar } from './elements/navBar';
+import Graph from "./elements/graph"
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <EditBar />
+      <Graph />
     </div>
   );
 }
