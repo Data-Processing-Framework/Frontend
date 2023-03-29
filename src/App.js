037bec82a17a3d632de13b3510b553b4e0cecdea
martilopez <marti.lopez.p@gmail.com>
2023-03-29 11:29:32 +0200

    UpdateEditState

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import { NavBar } from './elements/navBar';
 import Graph from "./elements/graph"
 import { Info }  from './elements/info';
 import React, {useState} from 'react';
+import { useStore } from 'reactflow';
 
 function App() {
+  //Handles if info section is visible or not
   const [infoIsOpen, setInfoIsOpen] = useState(false);
   const handleToggleIngfo = () => {
     setInfoIsOpen(!infoIsOpen)
   }
+
+  //Handles if app is in edit mode or not
+  const [editMode, setEditMode] = useState(false);
+  const handleEditState = () => {
+    setEditMode(!editMode)
+  }
+
   return (
     <div className={`App ${infoIsOpen ? 'open' : ''}`}>
       <NavBar toggleIngfo={handleToggleIngfo}/>
