f4a078f1df811a2e742be6ece99db7ff69a54c59
martilopez <63054913+martilopez@users.noreply.github.com>
2023-04-02 21:14:38 +0200

    Moduls

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import './App.css';
 import { EditBar } from './elements/editBar';
 import {NavBar} from './elements/navBar'
@@ -7,6 +8,48 @@ function App() {
     <div className="App">
       <NavBar />
       <EditBar />
+=======
+import './App.sass';
+import './elements/css/colorPalette.sass'
+import { SecondaryBar } from './elements/secondaryBar';
+import { NavBar } from './elements/navBar';
+import Graph from "./elements/graph"
+import {ShowModuls} from "./elements/showModuls"
+import { Info }  from './elements/info';
+import React, {useState} from 'react';
+import { useStore } from 'reactflow';
+
+function App() {
+  //Handles if info section is visible or not
+  const [infoIsOpen, setInfoIsOpen] = useState(false);
+  const handleToggleInfo = () => {
+    setInfoIsOpen(!infoIsOpen)
+  }
+
+  //Handles if moduls section is visible or not
+  const [modulsIsOpen, setModulsIsOpen] = useState(false);
+  const handleToggleModuls = () => {
+    setModulsIsOpen(!modulsIsOpen)
+  }
+
+  //Handles if app is in edit mode or not
+  const [editMode, setEditMode] = useState(false);
+  const handleEditState = () => {
+    setEditMode(!editMode)
+  }
+
+  return (
+    <div className='App'>
+      <NavBar 
+        toggleInfo={handleToggleInfo} 
+        toggleMode={handleEditState} 
+        toggleModuls={handleToggleModuls}
+      />
+      <SecondaryBar isOpen={infoIsOpen} mode={editMode}/>
+      <Info isOpen={infoIsOpen}/>
+      {modulsIsOpen && <ShowModuls toggleModuls={handleToggleModuls}/>}
+      <Graph />
+>>>>>>> Stashed changes
     </div>
   );
 }
--- src/elements/navBar.js
+++ src/elements/navBar.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import './navBar.css'
 import logo from '../img/logo.png'
 
@@ -7,6 +8,22 @@ export function NavBar() {
         <div className="leftNav">
           <img src={logo} alt="logo"/>
           <h1> File Name</h1>
+=======
+import './css/navBar.sass'
+//import logo from '../img/logo.png'
+import './css/modulButton.sass'
+
+export function NavBar({toggleInfo, toggleMode, toggleModuls}) {
+    return (
+      <nav className='navBar'>
+        <div className="leftNav">
+          <h1> Data Processing Framework </h1>
+        </div>
+        <div className="rightNav">
+          <button onClick={toggleMode}>Mode</button>
+          <button onClick={toggleInfo}>Info</button>
+          <button onClick={toggleModuls}>Moduls</button>
+>>>>>>> Stashed changes
         </div>
       </nav>
     );
