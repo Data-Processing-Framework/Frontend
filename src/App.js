ca649ef86c696069c92245674a16503c164f347b
martilopez <63054913+martilopez@users.noreply.github.com>
2023-04-24 18:51:23 +0200

    Start/stop

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ import { NavBar } from './elements/navBar';
 import Graph from "./elements/graph"
 import {ShowModuls} from "./elements/showModuls"
 import { Info }  from './elements/info';
-import React, {useState} from 'react';
-import { useStore } from 'reactflow';
-import { Alert } from './elements/alerts';
+import React, {useState, useEffect} from 'react';
+import  Alert from './elements/alerts';
 
 function App() {
   //Handles if info section is visible or not
@@ -36,19 +35,53 @@ function App() {
   }
 
   //Handles if app is in edit mode or not
-  const [editMode, setEditMode] = useState(false);
-  const handleEditState = () => {
-    setEditMode(!editMode)
+  //TODO Change graph settings when in edit mode or not 
+  const [editMode, setEditMode] = useState(true);
+  const sysStop = () => {
+    setEditMode(true)
+    //TODO fetch /system/stop
+    //TODO get /sytem/status
+    fetch('https://virtserver.swaggerhub.com/BIELCAMPRUBI/DPF/1/system/status')
+      .then((response) => {console.log(response); return response.json()})
+      .then((json) => {
+        console.log(json);
+        json.errors.forEach(printError)
+      });
+
   }
+  const sysStart = () => {
+    setEditMode(false)
+    //TODO put graph
+    //TODO fetch /system/start
+    //TODO get /sytem/status
+  }
+
+  const sysRestart = () => {
+    //TODO fetch /system/restart
+    //TODO get /sytem/status
+  }
+
+  
 
   return (
     <div className='App'>
       <NavBar 
-        toggleMode={handleEditState} 
+        sysStart={sysStart}
+        sysStop={sysStop}
+        sysRestart={sysRestart}
+        editMode = {editMode}
+
         toggleModuls={handleToggleModuls}
       />
-      <SecondaryBar isOpen={infoNode} mode={editMode}/>
-      <Info open={infoOpen}node={infoNode} closeInfo={closeInfo}/>
+      <SecondaryBar 
+        isOpen={infoOpen} 
+        mode={editMode}
+      />
+      <Info 
+        open={infoOpen} 
+        node={infoNode} 
+        closeInfo={closeInfo}
+      />
       {modulsIsOpen && <ShowModuls toggleModuls={handleToggleModuls}/>}
       <Graph 
         setSelectedNode={setInfo} 
@@ -56,9 +89,12 @@ function App() {
         closeInfo={closeInfo} 
         openInfo={openInfo}
       />
-      <Alert />
+      <Alert/>
     </div>
   );
 }
-
 export default App;
+
+function printError(error) {
+  <Alert error={error} /> ;
+}
--- src/elements/navBar.js
+++ src/elements/navBar.js
@@ -2,16 +2,36 @@ import './css/navBar.sass'
 //import logo from '../img/logo.png'
 import './css/modulButton.sass'
 
-export function NavBar({toggleInfo, toggleMode, toggleModuls}) {
+export function NavBar({sysStart, sysStop, sysRestart, editMode, toggleModuls}) {
     return (
       <nav className='navBar'>
         <div className="leftNav">
           <h2 className='mb-0'> Data Processing Framework </h2>
         </div>
         <div className="rightNav">
-          <button onClick={toggleMode}>Mode</button>
-          <button onClick={toggleModuls}>Moduls</button>
+          {editMode && <InactiveNav sysStart={sysStart}/>}
+          {!editMode && <ActiveNav sysStop={sysStop} sysRestart={sysRestart}/>}
+          <div className='modulsHandlers'>
+            <button onClick={toggleModuls}>Moduls</button>
+          </div>
         </div>
       </nav>
     );
+}
+
+export function ActiveNav ({sysStop, sysRestart}) {
+  return(
+    <div className='stateHandlers'>
+      <button onClick={sysRestart}>Restart</button>
+      <button onClick={sysStop}>Stop</button>
+    </div>
+  )
+}
+
+export function InactiveNav ({sysStart}) {
+  return(
+    <div className='stateHandlers'>
+      <button onClick={sysStart}>Start</button>
+    </div>
+  )
 }
